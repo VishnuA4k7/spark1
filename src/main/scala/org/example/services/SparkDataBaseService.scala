3a5c6730656433d1b07714711a7a9295863e3ee1
vishn_fnicyap <vishnu4k7@gmail.com>
2023-12-24 19:17:00 +0530

    commiting All files to the local_branch1 git..2

--- src/main/scala/org/example/services/SparkDataBaseService.scala
+++ src/main/scala/org/example/services/SparkDataBaseService.scala
@@ -7,6 +7,7 @@ import org.example.{ApplicationConfig, InitSpark}
 
 class SparkDataBaseService (config: ApplicationConfig) extends InitSpark{
 
+  // test
   val spark = getSpark(config.AppName)
   import spark.implicits._
 
